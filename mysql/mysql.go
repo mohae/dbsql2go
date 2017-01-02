2dda2f204086e2b53e490cd4a179092dc0d1349f
Joel Scoble <joel.scoble@outlook.com>
2017-01-02 16:11:49 -0600

    add and update LICENSE info

--- mysql/mysql.go
+++ mysql/mysql.go
@@ -1,4 +1,4 @@
-// Copyright 2016 Joel Scoble.
+// Copyright 2016-17 Joel Scoble.
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
